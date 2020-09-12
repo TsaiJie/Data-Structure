f0c2c205fffd3720cb08863f964998c6ef372bc1
tsai <mengjietsai@163.com>
2020-09-12 22:43:52 +0800

    添加虚拟头节点

--- tsVersion/LinkedList/linkedList.ts
+++ tsVersion/LinkedList/linkedList.ts
@@ -13,11 +13,11 @@ class ListNode<E> {
 }
 
 class LinkedList<E> {
-  private head: ListNode<E>;
+  private dummyHead: ListNode<E>;
   private size: number;
   
   constructor() {
-    this.head = null;
+    this.dummyHead = new ListNode<E>(null, null);
     this.size = 0;
   }
   
@@ -28,36 +28,35 @@ class LinkedList<E> {
   public isEmpty(): boolean {
     return this.size === 0;
   }
-  
-  public addFirst(e: E): void {
-    // const node = new ListNode(e)
-    // node.next = this.head;
-    // this.head = node;
-    this.head = new ListNode<E>(e, this.head)
-    this.size++;
-  }
+
 
 // 在链表的第index的位置添加新的元素e
   public add(index: number, e: E) {
     if (index < 0 || index > this.size) {
       throw new Error("Add failed. Illegal index");
     }
-    if (index === 0) {
-      this.addFirst(e);
-    } else {
-      let prev = this.head;
-      for (let i = 0; i < index - 1; i++) {
-        prev = prev.next;
-      }
-      // const node = new ListNode<E>(e);
-      // node.next = prev.next;
-      // prev.next = node;
-      prev.next = new ListNode<E>(e, prev.next)
-      this.size++;
+    
+    let prev = this.dummyHead;
+    for (let i = 0; i < index; i++) {
+      prev = prev.next;
     }
+    // const node = new ListNode<E>(e);
+    // node.next = prev.next;
+    // prev.next = node;
+    prev.next = new ListNode<E>(e, prev.next)
+    this.size++;
+    
   }
+  
   // 在链表最后插入元素
-  public addLast(e: E):void {
+  public addLast(e: E): void {
     this.add(this.size, e);
+  }
+  
+  public addFirst(e: E): void {
+    // const node = new ListNode(e)
+    // node.next = this.head;
+    // this.head = node;
+    this.add(0, e);
   }
 }
