ed6f26e2791a4638d05beb7cbb32e06be5d381de
tsai <mengjietsai@163.com>
2020-10-06 15:38:29 +0800

    二分搜索树添加元素

---
+++ tsVersion/Tree/BST.ts
@@ -0,0 +1,68 @@
+class TreeNode {
+  public e: number;
+  public left: TreeNode;
+  public right: TreeNode;
+  
+  constructor(e: number) {
+    this.e = e;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BST {
+  private root: TreeNode;
+  private size: number;
+  
+  constructor() {
+    this.root = null;
+    this.size = 0;
+  }
+  
+  public getSize(): number {
+    return this.size
+  }
+  
+  public isEmpty(): boolean {
+    return this.size === 0;
+  }
+  
+  // 向二分搜索树中添加新的元素e
+  
+  public add(e: number): void {
+    if (this.root === null) {
+      this.root = new TreeNode(e);
+      this.size++;
+    } else {
+      this.addWithNode(this.root, e);
+    }
+  }
+  
+  // 向以node为根的二分搜索树中插入元素e，递归
+  private addWithNode(node: TreeNode, e: number): void {
+    // 当e和当前节点相等时直接return
+    if (e === node.e) {
+      return;
+    } else if (e < node.e && node.left === null) {
+      // 当e小于当前节点，并且当前节点的左孩子为空时
+      node.left = new TreeNode(e);
+      this.size++;
+      return;
+    } else if (e > node.e && node.right === null) {
+      // 当e大于当前节点，并且当前节点的右孩子为空时
+      node.right = new TreeNode(e);
+      this.size++;
+      return;
+    }
+    if (e < node.e) {
+      // 当e小于当前节点，并且左孩子不为空时，继续往下查找
+      this.addWithNode(node.left, e);
+    } else {
+      // 当e大于当前节点，并且右孩子不为空时，继续往下查找
+      this.addWithNode(node.right, e);
+    }
+    
+    
+  }
+  
+}
